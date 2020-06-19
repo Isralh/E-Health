4d6e7c1f70c415a3e49af91e02e011a0e2b820d7
Isral <IsralHadero@gmail.com>
2020-06-19 18:08:44 -0400

    change to gitignore

--- server/src/Config/AwsConfig.js
+++
@@ -1,7 +0,0 @@
-const awsconfig = {
-  bucketName: 'e-health-bucket',
-  region: 'us-east-2',
-  accessKeyId: 'AKIA5YXLRDPRU3TTHRYF',
-  secretAccessKey: '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'
-}
-module.exports = awsconfig
--- server/src/Database/mysqlDB.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize').Sequelize
-require('dotenv').config()
-module.exports = new Sequelize('e_health', 'root', '1992', {
-  host: 'localhost',
-  dialect: 'mysql'
-})
