a865fa118cd7034e6e35bfaaaff1c3eaa62d792d
Isral <IsralHadero@gmail.com>
2020-06-24 22:48:31 -0400

    date and time complete

--- client/src/Components/Payment/DateAndTime.js
+++ client/src/Components/Payment/DateAndTime.js
@@ -1,10 +1,35 @@
-import React from 'react'
-
-const DateAndTime = () => {
+import React, { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+const DateAndTime = ({ handleTime, handleDate, selection, times = [], dateFilter }) => {
   return (
     <div className='dateTime-container'>
-      <div className='topheading'>
-        <h1><span className='step-one'>1.</span><span className='heading'>Choose date and time</span></h1>
+      <div className='top-heading'>
+        <h1><span className='step-one'>1.</span><span className='heading'>Choose Date and Time</span></h1>
+      </div>
+      <div className='datePicker'>
+        <p> Select Date: </p>
+        <DatePicker
+          selected={selection}
+          onChange={handleDate}
+          filterDate={dateFilter}
+          minDate={new Date()}
+          placeholderText='Available Dates'
+          isClearable
+          className='selected-date'
+        />
+        <p> Select Time:</p>
+        <select className='selected-time' onChange={handleTime}>
+          {times.map((time, i) =>
+            <option key={i}>{time}</option>
+          )}
+        </select>
+        <p>Session Length:</p>
+        <select className='selected-time '>
+          <option>1 hour</option>
+          <option>2 hours</option>
+          <option>3 hours</option>
+        </select>
       </div>
     </div>
   )
--- client/src/Components/Payment/Payment.js
+++ client/src/Components/Payment/Payment.js
@@ -7,17 +7,53 @@ import './styles.scss'
 import Modal from '../Appointment/Modal/Modal'
 import DateAndTime from './DateAndTime'
 const Payment = () => {
+  /* initial modal status is false, on view profile click it will be true and modal will be open */
   const [modalStatus, setModalStatus] = useState(false)
 
+  /* current selected doctor information */
   const [doctorInfo, setDoctorInfo] = useState()
 
+  /* state to hold booking selection date */
+  const [bookingDate, setBookingDate] = useState()
+
+  /* state to hold the selected booking time */
+  const [bookingTime, setBookingTime] = useState()
+
+  /* Available booking time */
+  const [availableTimes, setAvailableTimes] = useState(
+    ['8 AM', '9AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'])
+
+  /* filter out the weekends so user can't book */
+  const filterDates = (date) => {
+    return date.getDay() !== 6 && date.getDay() !== 0
+  }
+
+  /* get the booking date from the customer */
+  const getBookingDate = (date) => {
+    setBookingDate(date)
+  }
+
+  /* function to get the booking time from the customer */
+
+  const getBookingTime = (e) => {
+    setBookingTime(e.target.value)
+  }
+
+  useEffect(() => {
+    console.log(bookingDate)
+    console.log(bookingTime)
+  }, [bookingDate, bookingTime])
+  /* set the doctors rating based on their rate... this is a mock rating system */
   const doctorsRating = () => {
     return doctorInfo.rating < 120 ? <FourStar /> : <FiveStar />
   }
 
+  /* set the doctors rating based on their rate... this is a mock rating system */
   const numberRating = () => {
     return doctorInfo.rating < 120 ? '4.0' : '5.0'
   }
+
+  /* fetch the doctor's info on initial load to show in the doctor's card */
   useEffect(() => {
     SharedServices()
       .then(res => {
@@ -36,6 +72,7 @@ const Payment = () => {
   const modalClose = () => {
     setModalStatus(false)
   }
+
   return (
     <div className='payment-container'>
       <NavBar />
@@ -56,7 +93,13 @@ const Payment = () => {
             closeModal={modalClose}
           />
         </> : null}
-      <DateAndTime />
+      <DateAndTime
+        times={availableTimes}
+        dateFilter={filterDates}
+        handleDate={getBookingDate}
+        selection={bookingDate}
+        handleTime={getBookingTime}
+      />
     </div>
   )
 }
