38fffdbaf311e1fff92a31b1705e456017c729ea
Isral <IsralHadero@gmail.com>
2020-07-08 01:37:00 -0400

    our doctors section complete

--- client/src/Components/Home/OurDoctors/OurDoctors.js
+++ client/src/Components/Home/OurDoctors/OurDoctors.js
@@ -2,13 +2,26 @@ import React from 'react'
 import { ErickKnight, ZachSmith } from '../../../Assets/Images/exports'
 import './Styles.scss'
 import { BookAppointment } from '../Buttons/Buttons'
+
 const OurDoctors = () => {
   return (
     <div className='ourDoctors-container'>
       <div className='OurDoctors-heading'>
-        <h1>Our experienced doctors</h1>
+        <h1>OUR EXPERIENCED DOCTORS</h1>
       </div>
       <div className='ourDoctors-list'>
+        <div className='ourDoctors'>
+          <img src={ErickKnight} alt='ErickKnight' className='docImg' />
+          <p className='name'>Dr. Erick Knight</p>
+          <p className='reviews'>View Profile & Reviews</p>
+          <BookAppointment />
+        </div>
+        <div className='ourDoctors'>
+          <img src={ZachSmith} alt='ErickKnight' className='docImg' />
+          <p className='name'>Dr. Erick Knight</p>
+          <p className='reviews'>View Profile & Reviews</p>
+          <BookAppointment />
+        </div>
         <div className='ourDoctors'>
           <img src={ErickKnight} alt='ErickKnight' className='docImg' />
           <p className='name'>Dr. Erick Knight</p>
