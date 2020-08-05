2ee0a3e783469ec2cd8b2882322e0ded1aa63bc0
Isral <IsralHadero@gmail.com>
2020-08-04 22:55:20 -0400

    review updates

--- client/src/Components/Home/ClientReviews/Reviews.js
+++ client/src/Components/Home/ClientReviews/Reviews.js
@@ -11,7 +11,7 @@ export const FirstReview = () => {
       <div className='review-wrapper'>
         <h1>Isral Hadero</h1>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         </p>
         <ReviewStars />
       </div>
@@ -28,7 +28,7 @@ export const SecondReview = () => {
       <div className='review-wrapper'>
         <h1>Isral Hadero</h1>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         </p>
         <ReviewStars />
       </div>
@@ -45,7 +45,7 @@ export const ThirdReview = () => {
       <div className='review-wrapper'>
         <h1>Isral Hadero</h1>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         </p>
         <ReviewStars />
       </div>
